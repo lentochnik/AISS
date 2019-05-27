786d8051a5e64c622b7804b8f753305b56bb1a2e
Олег <olegexe@bk.ru>
2019-05-27 16:32:32 +0300

    27/05/2019

---
+++ AIS/Data/Param.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace AIS
+{
+    class Param
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = Properties.Settings.Default.host;
+            int port = Properties.Settings.Default.port;
+            string database = Properties.Settings.Default.db;
+            string username = Properties.Settings.Default.name;
+            string password = Properties.Settings.Default.pass;
+
+            return Msqlc.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
